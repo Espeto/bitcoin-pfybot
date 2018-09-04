81916a7d2d28660c0e6ceb41a2a27098b524b026
intrd <contact@dann.com.br>
2018-09-04 00:19:19 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@
 # @author intrd - http://dann.com.br/ 
 # @license Creative Commons Attribution-ShareAlike 4.0 International License - http://creativecommons.org/licenses/by-sa/4.0/
 
-
 while :
 do
 	echo "Press [CTRL+C] to stop.."
